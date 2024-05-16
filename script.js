775a5297a6e58f7ca2cc1d9c3e01dc17fb588e52
Ashvin <ashvinjangid10@gmail.com>
2024-05-16 17:09:27 +0530

    implement basic calculator logic

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function add(num1, num2) {
 	return num1 + num2;
 };
 
-function multiply(num1, num2) {
+function subtract(num1, num2) {
 	return num1 - num2;
 };
 
@@ -13,3 +13,37 @@ function divide(num1, num2) {
 function multiply(num1, num2) {
 	return num1 * num2;
 };
+
+function operate(operator, num1, num2) {
+	num1 = Number(num1);
+	num2 = Number(num2);
+	switch (operator) {
+		case '+':
+			return add(num1, num2);
+		case '-':
+			return subtract(num1, num2);
+		case '/':
+			return divide(num1, num2);
+		case '*':
+			return multiply(num1, num2);
+	}
+}
+
+const buttons = document.querySelectorAll('button');
+const evaluateBtn = document.querySelector('#evaluate')
+const display = document.querySelector('.display');
+const displayText = document.createElement('span');
+display.appendChild(displayText);
+
+buttons.forEach(button => {
+	button.addEventListener('click', (e) => {
+		displayText.textContent += e.target.textContent;
+	});
+});
+
+evaluateBtn.addEventListener('click', () => {
+	const string = displayText.textContent;
+	const numbers = string.match(/[0-9]+/g);
+	const operator = string.match(/\D/);
+	displayText.textContent = operate(operator[0], numbers[0], numbers[1]);
+});
